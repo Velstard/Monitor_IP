b691eabe48b29bc22f5e5a7727fc8a6aa7d863b9
Velstard <javierenriques12345@hotmail.com>
2025-04-21 20:36:57 -0400

    Create db.sql

---
+++ db.sql
@@ -0,0 +1,20 @@
+
+CREATE TABLE IF NOT EXISTS public.users
+(
+    id integer NOT NULL DEFAULT nextval('users_id_seq'::regclass),
+    username character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    email character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    password character varying(200) COLLATE pg_catalog."default" NOT NULL,
+    role character varying(20) COLLATE pg_catalog."default" NOT NULL DEFAULT 'viewer'::character varying,
+    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    token character varying(255) COLLATE pg_catalog."default",
+    approved boolean DEFAULT false,
+    CONSTRAINT users_pkey PRIMARY KEY (id),
+    CONSTRAINT users_email_key UNIQUE (email),
+    CONSTRAINT users_username_key UNIQUE (username)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.users
+    OWNER to postgres;
